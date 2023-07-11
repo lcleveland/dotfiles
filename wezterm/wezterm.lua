45487877a43521231a0589c14aa3a0623a4d3f8d
Lyle Cleveland <lyle.cleveland@yahoo.com>
2023-07-11 09:06:05 -0500

    Update

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -5,5 +5,10 @@ if wezterm.config_builder then
 end
 config.font = wezterm.font_with_fallback {
 	'Hack NF FC Ligatured',
+	'FiraCode Nerd Font',
+	'Symbols Nerd Font',
+	'Noto Color Emoji',
 }
+config.hide_tab_bar_if_only_one_tab = true
+config.window_background_opacity = 0.8
 return config
